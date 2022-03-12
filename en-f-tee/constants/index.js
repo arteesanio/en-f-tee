655b406c4f7975732135c1672ef00ae419196aae
arteesan <gamearteesan@gmail.com>
2022-03-12 14:33:12 -0400

    upgrade

--- en-f-tee/constants/index.js
+++ en-f-tee/constants/index.js
@@ -3,4 +3,6 @@ const WHITELIST_CONTRACT_ADDRESS = "0x6c8fdAaEaB22D19C468A885e989d2206404bf638";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://en-f-tee-38rd5n11i-arteesanio.vercel.app/api/";
 
-module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+const NFT_CONTRACT_ADDRESS = "0x6731E52121DdB63585738d27ec8bdb5857bC78BA";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL, NFT_CONTRACT_ADDRESS };
