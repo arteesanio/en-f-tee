d375e14470c5a28b601701b313184a7b5b0556a1
arteesan <gamearteesan@gmail.com>
2022-03-12 13:58:49 -0400

    first commit

---
+++ en-f-tee/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x6c8fdAaEaB22D19C468A885e989d2206404bf638";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
